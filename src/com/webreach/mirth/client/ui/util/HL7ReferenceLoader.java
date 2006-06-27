fe5f1b462d47e329aed0075b1d9477a6182e7290
gerald_bortis <gerald_bortis@c801cdd1-710f-0410-a44a-8d9edceeb29b>
2006-06-27 00:38:40 +0000

    Added data, classes, and jar needed for loaded HL7 segment reference.
    git-svn-id: https://svn.mirthcorp.com/connect/branches/client@272 c801cdd1-710f-0410-a44a-8d9edceeb29b

---
+++ src/com/webreach/mirth/client/ui/util/HL7ReferenceLoader.java
@@ -0,0 +1,32 @@
+package com.webreach.mirth.client.ui.util;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.StringReader;
+
+import com.Ostermiller.util.CSVParser;
+
+public class HL7ReferenceLoader {
+	public String[][] getReferenceTable() {
+		String[][] values = null;
+
+		try {
+			File referenceFile = new File("reference.csv");
+			BufferedReader reader = new BufferedReader(new FileReader(referenceFile));
+			StringBuilder contents = new StringBuilder();
+			String line = null;
+
+			while ((line = reader.readLine()) != null) {
+				contents.append(line + "\n");
+			}
+
+			values = CSVParser.parse(new StringReader(contents.toString()));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return values;
+	}
+}
