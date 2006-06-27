5c091812c7a3fa0347a5fd9450278ed7604b279b
gerald_bortis <gerald_bortis@c801cdd1-710f-0410-a44a-8d9edceeb29b>
2006-06-27 22:36:35 +0000

    Added FileUtil.
    git-svn-id: https://svn.mirthcorp.com/connect/branches/client@284 c801cdd1-710f-0410-a44a-8d9edceeb29b

---
+++ src/com/webreach/mirth/client/ui/util/FileUtil.java
@@ -0,0 +1,30 @@
+package com.webreach.mirth.client.ui.util;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileUtil {
+	public void write(File file, String data) throws IOException {
+		BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+		writer.write(data);
+		writer.flush();
+		writer.close();
+	}
+
+	public String read(File file) throws IOException {
+		BufferedReader reader = new BufferedReader(new FileReader(file));
+		StringBuilder contents = new StringBuilder();
+		String line = null;
+
+		while ((line = reader.readLine()) != null) {
+			contents.append(line + "\n");
+		}
+		
+		reader.close();
+		return contents.toString();
+	}
+}
