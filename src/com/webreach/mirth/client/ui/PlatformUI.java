f076c7eff39993a991a88e05d4b1e8267dab075d
jacob_brauer <jacob_brauer@c801cdd1-710f-0410-a44a-8d9edceeb29b>
2006-06-28 22:48:21 +0000

    Added PlatformUI.java, which holds global static variables.
    git-svn-id: https://svn.mirthcorp.com/connect/branches/client@311 c801cdd1-710f-0410-a44a-8d9edceeb29b

---
+++ src/com/webreach/mirth/client/ui/PlatformUI.java
@@ -0,0 +1,11 @@
+package com.webreach.mirth.client.ui;
+
+import javax.swing.ImageIcon;
+
+public class PlatformUI 
+{
+    public static Frame MIRTH_FRAME;
+    public static ImageIcon BACKGROUND_IMAGE;
+    public static String SERVER_NAME;
+    public static String USER_NAME;
+}
