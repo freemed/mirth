d4f1dcb5dc4faf330d5522e1c6a7e1c2d8c98b1d
gerald_bortis <gerald_bortis@c801cdd1-710f-0410-a44a-8d9edceeb29b>
2006-07-06 19:23:54 +0000

    Optimized code to use FileUtil instead of reading in contents.
    git-svn-id: https://svn.mirthcorp.com/connect/branches/client@430 c801cdd1-710f-0410-a44a-8d9edceeb29b

--- src/com/webreach/mirth/client/ui/util/HL7ReferenceLoader.java
+++ src/com/webreach/mirth/client/ui/util/HL7ReferenceLoader.java
@@ -1,25 +1,12 @@
 package com.webreach.mirth.client.ui.util;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
-import java.io.StringReader;
 
 import com.Ostermiller.util.CSVParser;
 
 public class HL7ReferenceLoader {
 	public String[][] getReferenceTable() throws IOException {
-		File referenceFile = new File("reference.csv");
-		BufferedReader reader = new BufferedReader(new FileReader(referenceFile));
-		StringBuilder contents = new StringBuilder();
-		String line = null;
-
-		while ((line = reader.readLine()) != null) {
-			contents.append(line + "\n");
-		}
-
-		reader.close();
-		return CSVParser.parse(new StringReader(contents.toString()));
+		return CSVParser.parse(FileUtil.read(new File("reference.csv")));
 	}
 }
