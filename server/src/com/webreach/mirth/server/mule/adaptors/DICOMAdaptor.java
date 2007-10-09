97e2a68627aab209179b6f2b19094590a0d28656
dans <dans@c801cdd1-710f-0410-a44a-8d9edceeb29b>
2007-10-09 17:50:24 +0000

    git-svn-id: https://svn.mirthcorp.com/connect/branches/1.7.0@2798 c801cdd1-710f-0410-a44a-8d9edceeb29b

--- server/src/com/webreach/mirth/server/mule/adaptors/DICOMAdaptor.java
+++ server/src/com/webreach/mirth/server/mule/adaptors/DICOMAdaptor.java
@@ -1,10 +1,15 @@
 package com.webreach.mirth.server.mule.adaptors;
 
 import com.webreach.mirth.model.MessageObject;
+import com.webreach.mirth.model.Attachment;
 import com.webreach.mirth.model.converters.IXMLSerializer;
 import com.webreach.mirth.model.converters.SerializerFactory;
+import com.webreach.mirth.model.converters.DICOMSerializer;
+import com.webreach.mirth.server.util.UUIDGenerator;
+import com.webreach.mirth.server.controllers.MessageObjectController;
 
 import java.util.Map;
+import java.util.Iterator;
 
 /**
  * Created by IntelliJ IDEA.
@@ -14,23 +19,47 @@ import java.util.Map;
  * To change this template use File | Settings | File Templates.
  */
 public class DICOMAdaptor extends Adaptor {
-	protected void populateMessage() throws AdaptorException {
+    protected void populateMessage() throws AdaptorException {
 
-		messageObject.setRawDataProtocol(MessageObject.Protocol.DICOM);
-		messageObject.setTransformedDataProtocol(MessageObject.Protocol.XML);
-		messageObject.setEncodedDataProtocol(MessageObject.Protocol.DICOM);
+        messageObject.setRawDataProtocol(MessageObject.Protocol.DICOM);
+        messageObject.setTransformedDataProtocol(MessageObject.Protocol.XML);
+        messageObject.setEncodedDataProtocol(MessageObject.Protocol.DICOM);
 
-		try {
-			String message = serializer.toXML(source);
-			messageObject.setTransformedData(message);
-			populateMetadataFromXML(message);
-		} catch (Exception e) {
-			handleException(e);
-		}
-	}
+        try {
+            // Set transformed data
+            DICOMSerializer dSerializer = (DICOMSerializer) serializer;
+            String message = dSerializer.toXML(source);
+            messageObject.setTransformedData(message);
 
-	@Override
-	public IXMLSerializer<String> getSerializer(Map properties) {
-		return SerializerFactory.getSerializer(MessageObject.Protocol.DICOM, properties);
-	}
+            // Set rawdata on messageobject without attachment data
+            if(dSerializer.rawData != null)
+                messageObject.setRawData(dSerializer.rawData);
+            else
+                messageObject.setRawData("");
+            // Create attachment
+            MessageObjectController moc = MessageObjectController.getInstance();
+            if(dSerializer.pixelData != null && !dSerializer.pixelData.isEmpty()) {
+                Iterator i = dSerializer.pixelData.iterator();
+                while(i.hasNext()){
+                    String image = (String) i.next();
+                    Attachment attachment = new Attachment();
+                    attachment.setAttachmentId(UUIDGenerator.getUUID());
+                    attachment.setMessageId(messageObject.getId());
+                    attachment.setData(image.getBytes());
+                    attachment.setSize(image.length());
+                    attachment.setType("DICOM");
+                    moc.insertAttachment(attachment);
+                }
+                messageObject.setAttachment(true);
+            }
+            populateMetadataFromXML(message);
+        } catch (Exception e) {
+            handleException(e);
+        }
+    }
+
+    @Override
+    public IXMLSerializer<String> getSerializer(Map properties) {
+        return SerializerFactory.getSerializer(MessageObject.Protocol.DICOM, properties);
+    }
 }
